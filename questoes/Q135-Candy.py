4266a044ab10da17e56bf4cbc62382718b1cf24a
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2025-01-20 14:57:30 -0300

    Update Q135-Candy.py

--- questoes/Q135-Candy.py
+++ questoes/Q135-Candy.py
@@ -1,3 +1,7 @@
-
 def candy(ratings):
-    return len(ratings)
+    n = len(ratings)
+    candies = [1] * n 
+    for i in range(1, n):
+        if ratings[i] > ratings[i - 1]:
+            candies[i] = candies[i - 1] + 1
+    return sum(candies)
