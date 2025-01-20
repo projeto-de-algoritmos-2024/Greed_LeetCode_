e4f75efe70c92914e02e85615ca2aec6ca72cbee
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2025-01-20 15:51:39 -0300

    Update Q55-JumpGame.py

--- questoes/Q55-JumpGame.py
+++ questoes/Q55-JumpGame.py
@@ -1,16 +1,8 @@
 class Solution:
     def canJump(self, nums):
-        # Posição inicial
-        current_position = 0
-        
-        # Enquanto houverem saltos possíveis
-        while current_position < len(nums) - 1:
-            max_jump = nums[current_position]
-            # Se o salto atual for 0, ficamos presos
-            if max_jump == 0:
-                return False
-            # Move para o próximo índice
-            current_position += 1
-        
-        return True  # Supondo que sempre conseguimos chegar ao final
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                print(f"Parado no índice {i}, valor {nums[i]}")
+        return False
+
 
